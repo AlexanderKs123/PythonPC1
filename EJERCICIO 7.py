7744ae9c284349429de1683b497b84b60444fcdd
AlexanderKs123 <150602828+AlexanderKs123@users.noreply.github.com>
2023-11-18 17:19:07 +0000

    Subiendo Ejercicio 7

---
+++ EJERCICIO 7.py
@@ -0,0 +1,24 @@
+
+numero_1 = float(input("Ingresamos el primer número: "))
+numero_2 = float(input("Ingresamos el segundo número: "))
+
+# Menú de opciones
+print("Menú:")
+print("1. Imprime la suma de los dos números")
+print("2. Imprime la resta (primer número menos segundo número)")
+print("3. Imprime la multiplicación de los dos números")
+
+opcion = input("Ingresamos el número de la opción deseada (1, 2, o 3): ")
+
+# Realizar la operación correspondiente según la opción seleccionada
+if opcion == '1':
+    resultado = numero_1 + numero_2
+    print(f"La suma de {numero_1} y {numero_2} es: {resultado}")
+elif opcion == '2':
+    resultado = numero_1 - numero_2
+    print(f"La resta de {numero_1} y {numero_2} es: {resultado}")
+elif opcion == '3':
+    resultado = numero_1 * numero_2
+    print(f"La multiplicación de {numero_1} y {numero_2} es: {resultado}")
+else:
+    print("Opción no válida. Por favor, elija 1, 2 o 3.")
