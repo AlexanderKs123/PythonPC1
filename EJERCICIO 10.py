ee5bb14ce719c8f9b49673ac1ee28ca28e4f6dd8
AlexanderKs123 <150602828+AlexanderKs123@users.noreply.github.com>
2023-11-18 17:52:52 +0000

    Subiendo Ejercicio 10

---
+++ EJERCICIO 10.py
@@ -0,0 +1,10 @@
+
+lista_muestra = ['Rojo', 'Verde', 'Blanco', 'Negro', 'Rosa', 'Amarillo']
+
+# En esta parte eliminamos elementos en las posiciones 0, 4 y 5
+del lista_muestra[5]  
+del lista_muestra[4]  
+del lista_muestra[0]  
+
+# En esta parte se Imprime el resultado
+print("Resultado esperado:", lista_muestra)
