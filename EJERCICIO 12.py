14d5396f9c0433ea245761fc50cc7841e26bcd53
AlexanderKs123 <150602828+AlexanderKs123@users.noreply.github.com>
2023-11-18 18:23:25 +0000

    Subiendo Ejercicio 12

--- EJERCICIO 12.py
+++ EJERCICIO 12.py
@@ -1,8 +1,9 @@
-# En esta parte solicitamos el nombre_archivo
+# Solicitar al usuario el nombre del archivo
 nombre_archivo = input("Ingrese el nombre del archivo: ")
 
-# Obtener la extensión del archivo (sufijo) en minúsculas
-sufijo = nombre_archivo.lower().split('.')[-1]
+# Obtener el nombre del archivo y la extensión utilizando métodos de cadena
+nombre, extension = nombre_archivo.rsplit('.', 1)
+nombre = nombre.lower()  # Convertir a minúsculas para manejar sin importar mayúsculas/minúsculas
 
 # Definir un diccionario de tipos MIME
 tipos_mime = {
@@ -12,11 +13,17 @@ tipos_mime = {
     'png': 'image/png',
     'pdf': 'application/pdf',
     'txt': 'text/plain',
-    'zip': 'application/zip'
+    'aac': 'audio/aac',
+    'avi': 'video/x-msvideo',
+    'mp3': 'audio/mpeg',
+    'weba': 'audio/webm',
+    'webm': 'video/webm'
 }
 
 # En esta parte obtenemos el tipo MIME correspondiente o 'application/octet-stream' si no se encuentra
-tipo_mime = tipos_mime.get(sufijo, 'application/octet-stream')
+tipo_mime = tipos_mime.get(extension, 'application/octet-stream')
 
-# Aqui se imprime el resultado
+#En esta parte se muestra el resultado
+print(f"Nombre del archivo: {nombre}")
+print(f"Extensión del archivo: {extension}")
 print(f"Tipo MIME para {nombre_archivo}: {tipo_mime}")
