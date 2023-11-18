006f9aa86e9d3037cbc3002067331f25c2286b00
AlexanderKs123 <150602828+AlexanderKs123@users.noreply.github.com>
2023-11-18 17:31:42 +0000

    Subiendo Ejercicio 9

---
+++ EJERCICIO 9.py
@@ -0,0 +1,9 @@
+
+lista_original = ['Hola', 'buenos', 'días', 'profesor', 'compañeros']
+
+# En esta parte Invertimos la lista utilizando reverse()
+lista_invertida = list(reversed(lista_original))
+
+# En esta parte mostramos la lista invertida
+print("Lista Original:", lista_original)
+print("Lista Invertida:", lista_invertida)
